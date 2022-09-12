7e6f02f948262ad9375fa1421b866ed3bb67bfbd
Nikita <nik.hamaganov@gmail.com>
2022-09-12 12:15:38 +0800

    Lesson 14

---
+++ Second django course/MyPr/blog/templatetags/sidebar.py
@@ -0,0 +1,12 @@
+from atexit import register
+from django import template
+from blog.models import *
+
+register = template.Library()
+
+
+@register.inclusion_tag("blog/popular_posts.html")
+def get_popular(count=3):
+    posts = Post.objects.order_by('-views')[:count]
+
+    return {"posts": posts}
