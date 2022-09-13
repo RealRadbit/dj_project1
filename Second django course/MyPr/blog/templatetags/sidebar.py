757f144d95189cd5b54d7cfa2b1505b7e223456a
Nikita <nik.hamaganov@gmail.com>
2022-09-13 13:04:50 +0800

    Lesson 15

--- Second django course/MyPr/blog/templatetags/sidebar.py
+++ Second django course/MyPr/blog/templatetags/sidebar.py
@@ -10,3 +10,9 @@ def get_popular(count=3):
     posts = Post.objects.order_by('-views')[:count]
 
     return {"posts": posts}
+
+
+@register.inclusion_tag("blog/tags_tpl.html")
+def get_tags():
+    tags = Tag.objects.all()
+    return {"tags": tags}
